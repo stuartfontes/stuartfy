0f9672b670f573b255fd47d4ac14317bdbb815ce
Mauricio Stuart <mainjettdovava@hotmail.com>
2025-09-01 12:43:22 -0300

    Contin. do cód. baseado no curso de C# - Alura

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿// StuartFy
 
-string mensagemDeInicio = "\nBoas vindas ao StuartFy1, um aplicativo de músicas teste baseado em aprendizados de C#.";
+string mensagemDeInicio = "\nBoas vindas ao StuartFy, um aplicativo de músicas teste baseado em aprendizados de C#.";
+List<string> listaDasBandas = new List<string>{"u2","u3","u4"};
 
 // Uma das formas de isolar um código sem precisar copia-lo várias vezes é criando uma função 
 
-void ExibirMensagemDeInicio()
+void ExibirLogo()
 {
     Console.WriteLine(@"
 ░██████╗████████╗██╗░░░██╗░█████╗░██████╗░████████╗███████╗██╗░░░██╗
@@ -15,12 +16,13 @@ void ExibirMensagemDeInicio()
 ╚═════╝░░░░╚═╝░░░░╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░░░░░░░╚═╝░░░");
     Console.WriteLine(mensagemDeInicio);
 }
-ExibirMensagemDeInicio();
+
 // Void nao retorna nada, parenteses serve para indicar uma função, as chaves servem para encapsular
 // O @ é um Verbatim Literal, basicamente um termo para mostrar a string como ela irá ser vista na run
 
 void Exibir_menu()
 {
+    ExibirLogo();
     Console.WriteLine("\nDigite: 1 para registrar uma banda;");
     Console.WriteLine("Digite: 2 para mostrar todas as bandas;");
     Console.WriteLine("Digite: 3 para avaliar uma banda;");
@@ -31,12 +33,12 @@ void Exibir_menu()
     string opcaoEscolhida = Console.ReadLine()!;
     int opcaoEscolhida2 = int.Parse(opcaoEscolhida);
     // A ! é para o ReadLine não entregar uma opção nula
-
+    
     switch (opcaoEscolhida2)
     {
-        case 1: Console.WriteLine("Registre a banda desejada");
+        case 1: RegistrarBanda();
             break;
-        case 2: Console.WriteLine("Mostrando todas as bandas");
+        case 2: mostrarBandasRegistradas();
             break;
         case 3: Console.WriteLine("Avalie a banda selecionada");
             break;
@@ -46,9 +48,51 @@ void Exibir_menu()
             break;
         default: Console.WriteLine("Error 1: Opção inválida");
             break;
+    
+    }
+}
+    
+    void RegistrarBanda()
+    {
+        Console.Clear();
+        Console.WriteLine("**********************");
+        Console.WriteLine("\nRegistro de Bandas\n");
+        Console.WriteLine("**********************\n");
+        Console.Write("Digite o nome da banda que deseja registrar: ");
+        string nomeDaBanda = Console.ReadLine()!;
+        Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso" +
+                          $"\nRetornando ao menu;" +
+                          $"\nAguarde...");
+        listaDasBandas.Add(nomeDaBanda);
+        Thread.Sleep(2000);
+        Console.Clear();
+        Exibir_menu();
+    }
+
+    void mostrarBandasRegistradas()
+    {
+        Console.Clear();
+        Console.WriteLine("****************************");
+        Console.WriteLine("\nExibindo bandas, Aguardade:\n");
+        Console.WriteLine("*****************************");
+        //for (int i = 0; i < listaDasBandas.Count; i++)
+        //{
+           // Console.WriteLine($"Banda: {listaDasBandas[i]}");
+        // }
+        Console.WriteLine("\nAs bandas registradas são\n");
+        foreach (string banda in listaDasBandas)
+        {
+            Console.WriteLine($"{banda}\n");
+        }
+        Thread.Sleep(2000);
+        Console.WriteLine("Pressione uma tecla para retornar ao Menu: ");
+        Console.ReadKey();
+        Console.Clear();
+        Exibir_menu();
 
     }
 
-}
-Exibir_menu();
 
+    Exibir_menu();
+    
+    
